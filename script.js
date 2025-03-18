4f11d50cc2a4c55d9360f674b86a8e3531842e2d
Maxim <115576894+GitMaxm@users.noreply.github.com>
2025-03-18 16:04:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const postsContainer = document.getElementById('posts');
+
+    fetch('https://gitmaxm.github.io/posts-json/')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(post => {
+                const postElement = document.createElement('div');
+                postElement.innerHTML = `
+            <h2>${post.title}</h2>
+            <p><strong>Автор:</strong> ${post.author}</p>
+            <p>${post.body}</p>
+            <hr>
+          `;
+                postsContainer.appendChild(postElement);
+            });
+        })
+        .catch(error => {
+            console.error('Ошибка загрузки данных:', error);
+        });
+});
